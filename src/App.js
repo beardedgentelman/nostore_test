bde69b88ba3101117a3f55ef968d40085711707a
Andrii <girskiy.andrey@gmail.com>
2023-03-05 02:30:27 +0200

    adding private routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Route, Routes } from 'react-router-dom'
+import { useAuth0 } from '@auth0/auth0-react'
 import { clearStore } from 'actions/clearStoreAction'
 import Auth0ProviderWithHistory from 'auth/auth0Provider'
 import { FooterContainer, HeaderContainer, MainContainer } from 'containers'
 import { CartPage, MainPage, ProductSinglePage, ProductsPage, ProfilePage } from 'pages'
 import { AuthRoute } from 'utiles/AuthRoute'
-import { PrivateRoute } from 'utiles/PrivateRoute'
+import { PrivateRoutes } from 'utiles/PrivateRoutes'
 
 import { HeaderLogo, HeaderNav } from 'components'
 
@@ -34,15 +35,13 @@ function App() {
         </HeaderContainer>
         <MainContainer>
           <Routes>
-            {/* <Route element={<PrivateRoute />}> */}
-            <Route path='/' element={<MainPage />} />
-            <Route path='/products' element={<ProductsPage type='products' />} />
-            <Route path='/user_page' element={<ProfilePage type='user' />} />
-            <Route path='/cart' element={<CartPage type='user' />} />
-            {/* </Route> */}
-            {/* <Route element={<AuthRoute />}> */}
+            <Route path='/' element={<MainPage />} exact />
+            <Route element={<PrivateRoutes />}>
+              <Route path='/products' element={<ProductsPage type='products' />} />
+              <Route path='/user_page' element={<ProfilePage type='user' />} />
+              <Route path='/cart' element={<CartPage type='user' />} />
+            </Route>
             <Route path='/products/:productId' element={<ProductSinglePage type='products' />} />
-            {/* </Route> */}
           </Routes>
         </MainContainer>
         <FooterContainer></FooterContainer>
